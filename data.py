ff8f2bf7843bcf4a06a5438b58ae3ab0c155725d
MrFace55 <Serebryakov322@gmail.com>
2026-05-24 10:23:17 +0400

    color fix ver

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import os
 import random
 import sys
+from io import BytesIO
 
 import numpy as np
 from PIL import Image
@@ -18,10 +19,7 @@ def add_gaussian_noise(img: np.ndarray, sigma: float = 25) -> np.ndarray:
 def add_jpeg_compression(img: np.ndarray, quality: int = 75) -> np.ndarray:
     """Add JPEG compression artifacts to image."""
     pil_img = Image.fromarray(img.astype(np.uint8))
-    buffer = Image.BytesIO() if hasattr(Image, 'BytesIO') else None
-    if buffer is None:
-        from io import BytesIO
-        buffer = BytesIO()
+    buffer = BytesIO()
     pil_img.save(buffer, format='JPEG', quality=quality)
     buffer.seek(0)
     return np.array(Image.open(buffer))
@@ -34,7 +32,6 @@ def degrade_image(img: np.ndarray, degradation: str = 'gaussian', **kwargs) -> n
     elif degradation == 'jpeg':
         return add_jpeg_compression(img, quality=kwargs.get('quality', 75))
     elif degradation == 'mixed':
-        import random
         if random.random() < 0.5:
             return add_gaussian_noise(img, sigma=kwargs.get('sigma', 25))
         else:
@@ -43,10 +40,10 @@ def degrade_image(img: np.ndarray, degradation: str = 'gaussian', **kwargs) -> n
 
 
 class Provider(object):
-    def __init__(self, batch_size, num_workers, scale, path, patch_size, 
+    def __init__(self, batch_size, num_workers, scale, path, patch_size,
                  degradation='none', degradation_params=None):
-        self.data = DIV2K(scale, path, patch_size, degradation=degradation, 
-                         degradation_params=degradation_params)
+        self.data = DIV2K(scale, path, patch_size, degradation=degradation,
+                          degradation_params=degradation_params)
         self.batch_size = batch_size
         self.num_workers = num_workers
 
@@ -59,14 +56,16 @@ class Provider(object):
         return int(sys.maxsize)
 
     def build(self):
-        self.data_iter = iter(DataLoader(dataset=self.data, batch_size=self.batch_size, num_workers=self.num_workers,
-                                         shuffle=False, drop_last=False, pin_memory=False))
+        self.data_iter = iter(DataLoader(dataset=self.data, batch_size=self.batch_size,
+                                         num_workers=self.num_workers,
+                                         shuffle=True, drop_last=False, pin_memory=False))
 
     def next(self):
         if self.data_iter is None:
             self.build()
         try:
-            batch = self.data_iter.next()
+            # ИСПРАВЛЕНИЕ: используем next(), а не .next()
+            batch = next(self.data_iter)
             self.iteration += 1
             if self.is_cuda:
                 batch[0] = batch[0].cuda()
@@ -76,7 +75,8 @@ class Provider(object):
             self.epoch += 1
             self.build()
             self.iteration += 1
-            batch = self.data_iter.next()
+            # ИСПРАВЛЕНИЕ: используем next() здесь тоже
+            batch = next(self.data_iter)
             if self.is_cuda:
                 batch[0] = batch[0].cuda()
                 batch[1] = batch[1].cuda()
@@ -84,7 +84,7 @@ class Provider(object):
 
 
 class DIV2K(Dataset):
-    def __init__(self, scale, path, patch_size, rigid_aug=True, 
+    def __init__(self, scale, path, patch_size, rigid_aug=True,
                  degradation='none', degradation_params=None):
         super(DIV2K, self).__init__()
         self.scale = scale
@@ -93,20 +93,40 @@ class DIV2K(Dataset):
         self.path = path
         self.degradation = degradation
         self.degradation_params = degradation_params if degradation_params is not None else {}
-        self.file_list = [str(i).zfill(4)
-                          for i in range(1, 801)]  # TODO:use both train and valid (901)
+        self.file_list = [str(i).zfill(4) for i in range(1, 801)]
 
-        # need about 8GB shared memory "-v '--shm-size 8gb'" for docker container
+        # Проверка существования папки HR
+        dataHR = os.path.join(path, "HR")
+        if not os.path.exists(dataHR):
+            raise FileNotFoundError(f"Папка с изображениями не найдена: {dataHR}. "
+                                    f"Убедитесь, что структура папок: {path}/HR/*.png")
+
         self.hr_cache = os.path.join(path, "cache_hr.npy")
-        if not os.path.exists(self.hr_cache):
-            self.cache_hr()
-            print("HR image cache to:", self.hr_cache)
-        self.hr_ims = np.load(self.hr_cache, allow_pickle=True).item()
-        print("HR image cache from:", self.hr_cache)
 
-        # For color correction (upscale=1), we don't need LR cache - we generate degraded from HR
+        rebuild_cache = False
+        if not os.path.exists(self.hr_cache):
+            rebuild_cache = True
+        else:
+            try:
+                temp_cache = np.load(self.hr_cache, allow_pickle=True).item()
+                if len(temp_cache) < len(self.file_list):
+                    print(
+                        f"Кэш содержит только {len(temp_cache)} изображений, ожидается {len(self.file_list)}. Пересоздаю...")
+                    rebuild_cache = True
+            except:
+                rebuild_cache = True
+
+        if rebuild_cache:
+            self.cache_hr()
+            print("HR image cache created:", self.hr_cache)
+        else:
+            print("HR image cache loaded:", self.hr_cache)
+
+        self.hr_ims = np.load(self.hr_cache, allow_pickle=True).item()
+        print(f"Loaded {len(self.hr_ims)} HR images.")
+
         if self.scale == 1:
-            self.lr_ims = self.hr_ims  # Use HR as base, will apply degradation on-the-fly
+            self.lr_ims = self.hr_ims
         else:
             self.lr_cache = os.path.join(path, "cache_lr_x{}.npy".format(self.scale))
             if not os.path.exists(self.lr_cache):
@@ -118,35 +138,58 @@ class DIV2K(Dataset):
     def cache_lr(self):
         lr_dict = dict()
         dataLR = os.path.join(self.path, "LR", "X{}".format(self.scale))
+        if not os.path.exists(dataLR):
+            raise FileNotFoundError(f"Папка LR не найдена: {dataLR}")
         for f in self.file_list:
-            lr_dict[f] = np.array(Image.open(os.path.join(dataLR, f + "x{}.png".format(self.scale))))
+            fname = f + "x{}.png".format(self.scale)
+            fpath = os.path.join(dataLR, fname)
+            if os.path.exists(fpath):
+                lr_dict[f] = np.array(Image.open(fpath).convert('RGB'))
+            else:
+                print(f"Warning: LR file not found: {fpath}")
         np.save(self.lr_cache, lr_dict, allow_pickle=True)
 
     def cache_hr(self):
         hr_dict = dict()
         dataHR = os.path.join(self.path, "HR")
         for f in self.file_list:
-            hr_dict[f] = np.array(Image.open(os.path.join(dataHR, f + ".png")))
+            fname = f + ".png"
+            fpath = os.path.join(dataHR, fname)
+            if os.path.exists(fpath):
+                hr_dict[f] = np.array(Image.open(fpath).convert('RGB'))
+            else:
+                print(f"Warning: HR file not found: {fpath}")
         np.save(self.hr_cache, hr_dict, allow_pickle=True)
 
     def __getitem__(self, _dump):
         key = random.choice(self.file_list)
+
+        if key not in self.hr_ims:
+            available_keys = list(self.hr_ims.keys())
+            if not available_keys:
+                raise RuntimeError("Нет загруженных изображений в кэше!")
+            key = random.choice(available_keys)
+
         lb = self.hr_ims[key]
-        
-        # For color correction (scale=1), generate degraded image from HR on-the-fly
+
         if self.scale == 1:
-            im = lb.copy()  # Start with HR image
+            im = lb.copy()
             if self.degradation != 'none':
                 im = degrade_image(im, self.degradation, **self.degradation_params)
         else:
-            im = self.lr_ims[key]
+            if key not in self.lr_ims:
+                im = lb
+            else:
+                im = self.lr_ims[key]
 
         shape = im.shape
-        i = random.randint(0, shape[0] - self.sz)
-        j = random.randint(0, shape[1] - self.sz)
-        # c = random.choice([0, 1, 2])
 
-        # For scale=1, both input and output have the same size
+        if shape[0] < self.sz or shape[1] < self.sz:
+            i, j = 0, 0
+        else:
+            i = random.randint(0, shape[0] - self.sz)
+            j = random.randint(0, shape[1] - self.sz)
+
         if self.scale == 1:
             lb = lb[i:i + self.sz, j:j + self.sz, :]
             im = im[i:i + self.sz, j:j + self.sz, :]
@@ -159,11 +202,9 @@ class DIV2K(Dataset):
             if random.uniform(0, 1) < 0.5:
                 lb = np.fliplr(lb)
                 im = np.fliplr(im)
-
             if random.uniform(0, 1) < 0.5:
                 lb = np.flipud(lb)
                 im = np.flipud(im)
-
             k = random.choice([0, 1, 2, 3])
             lb = np.rot90(lb, k)
             im = np.rot90(im, k)
@@ -174,7 +215,8 @@ class DIV2K(Dataset):
         return im, lb
 
     def __len__(self):
-        return int(sys.maxsize)
+        # ИСПРАВЛЕНИЕ: возвращаем реальное количество изображений
+        return len(self.hr_ims)
 
 
 class SRBenchmark(Dataset):
@@ -182,7 +224,7 @@ class SRBenchmark(Dataset):
         super(SRBenchmark, self).__init__()
         self.ims = dict()
         self.files = dict()
-        _ims_all = 0  # Будем считать реальное количество изображений
+        _ims_all = 0
 
         for dataset in ['Set5', 'Set14', 'B100', 'Urban100', 'Manga109']:
             hr_folder = os.path.join(path, dataset, 'HR')
@@ -192,41 +234,44 @@ class SRBenchmark(Dataset):
             files = os.listdir(hr_folder)
             files.sort()
             self.files[dataset] = files
-            _ims_all += len(files) * 2  # Учитываем HR и LR для каждого файла
+            _ims_all += len(files) * 2
 
             for i in range(len(files)):
                 try:
                     # Загрузка HR изображения
                     hr_path = os.path.join(hr_folder, files[i])
-                    im_hr = np.array(Image.open(hr_path))
+                    im_hr = np.array(Image.open(hr_path).convert('RGB'))
                     im_hr = modcrop(im_hr, scale)
-                    if len(im_hr.shape) == 2:
-                        im_hr = np.expand_dims(im_hr, axis=2)
-                        im_hr = np.concatenate([im_hr, im_hr, im_hr], axis=2)
 
                     key = dataset + '_' + files[i][:-4]
                     self.ims[key] = im_hr
 
-                    # Загрузка LR изображения
+                    # Загрузка LR изображения (если нужно для сравнения, но для scale=1 мы генерируем сами)
+                    # Однако класс ожидает наличие ключа '...x{scale}' если используется старый код тестирования
+                    # Для нашей задачи (scale=1) мы будем игнорировать LR из датасета и генерировать на лету в test.py
                     lr_path = os.path.join(path, dataset, 'LR_bicubic', f'X{scale}', files[i][:-4] + f'x{scale}.png')
-                    im_lr = np.array(Image.open(lr_path))
 
-                    if len(im_lr.shape) == 2:
-                        im_lr = np.expand_dims(im_lr, axis=2)
-                        im_lr = np.concatenate([im_lr, im_lr, im_lr], axis=2)
+                    if os.path.exists(lr_path):
+                        im_lr = np.array(Image.open(lr_path).convert('RGB'))
+                        if len(im_lr.shape) == 2:
+                            im_lr = np.expand_dims(im_lr, axis=2)
+                            im_lr = np.concatenate([im_lr, im_lr, im_lr], axis=2)
 
-                    # Проверка и корректировка размеров
-                    if im_lr.shape[0] * scale != im_hr.shape[0]:
-                        expected_size = im_lr.shape[0] * scale
-                        im_hr = im_hr[:expected_size, :expected_size, :]
+                        # Проверка и корректировка размеров
+                        if im_lr.shape[0] * scale != im_hr.shape[0]:
+                            expected_size = im_lr.shape[0] * scale
+                            im_hr = im_hr[:expected_size, :expected_size, :]
 
-                    key = dataset + '_' + files[i][:-4] + f'x{scale}'
-                    self.ims[key] = im_lr
+                        key_lr = dataset + '_' + files[i][:-4] + f'x{scale}'
+                        self.ims[key_lr] = im_lr
+                    else:
+                        # Если LR нет, создадим заглушку или просто пропустим,
+                        # так как в test.py для scale=1 мы генерируем деградацию из HR
+                        pass
 
                 except Exception as e:
                     print(f"Error loading {files[i]}: {str(e)}")
-                    _ims_all -= 2  # Уменьшаем счетчик при ошибке загрузки
+                    _ims_all -= 2
                     continue
 
-        # Убираем жесткую проверку, просто выводим информацию
         print(f"Loaded {len(self.ims.keys())} images (expected about {_ims_all})")
--- train.py
+++ train.py
@@ -15,13 +15,18 @@ from models import *
 from data import Provider, SRBenchmark, degrade_image
 from utils import PSNR, _rgb2ycbcr, seed_everything
 
-
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def sanitize_name(name):
+    """Заменяет запрещенные символы Windows на подчеркивания"""
+    return name.replace(':', '_').replace('/', '_').replace('\\', '_')
+
 
 def parse_args():
     parser = argparse.ArgumentParser("Training Setting")
     parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--n-workers", type=int,  default=8)
+    parser.add_argument("--n-workers", type=int, default=8)
     parser.add_argument("--train-dir", type=str, default='./data/train/DIV2K',
                         help="Training images")
     parser.add_argument("--val-dir", type=str, default='./data/test/',
@@ -44,7 +49,7 @@ def parse_args():
     parser.add_argument("--train-iter", type=int, default=200000,
                         help="number of training iterations")
     parser.add_argument('--lr', type=float, default=5e-4, help="initial learning rate")
-    parser.add_argument('--wd', type=float, default=0,  help='weight decay')
+    parser.add_argument('--wd', type=float, default=0, help='weight decay')
 
     parser.add_argument('--degradation', type=str, default='gaussian',
                         choices=['gaussian', 'jpeg', 'mixed', 'none'],
@@ -53,7 +58,7 @@ def parse_args():
                         help='Gaussian noise standard deviation')
     parser.add_argument('--jpeg-quality', type=int, default=75,
                         help='JPEG compression quality')
-    
+
     parser.add_argument('--msb', type=str, default='hdb', choices=['hdb', 'hd'])
     parser.add_argument('--lsb', type=str, default='hd', choices=['hdb', 'hd'])
     parser.add_argument('--act-fn', type=str, default='relu', choices=['relu', 'gelu', 'leakyrelu', 'starrelu'])
@@ -61,8 +66,13 @@ def parse_args():
     args = parser.parse_args()
 
     factors = 'x'.join([str(s) for s in args.upscale])
-    args.exp_name = "msb:{}-lsb:{}-act:{}-nf:{}-{}-deg:{}".format(
+    # Создаем имя эксперимента
+    raw_exp_name = "msb:{}-lsb:{}-act:{}-nf:{}-{}-deg:{}".format(
         args.msb, args.lsb, args.act_fn, args.n_filters, factors, args.degradation)
+
+    # Сохраняем оригинальное имя для логов внутри файла, но используем безопасное для путей
+    args.exp_name = raw_exp_name
+    args.safe_exp_name = sanitize_name(raw_exp_name)
 
     act_fn_dict = {'relu': nn.ReLU, 'gelu': nn.GELU, 'leakyrelu': nn.LeakyReLU, 'starrelu': StarReLU}
     args.act_fn = act_fn_dict[args.act_fn]
@@ -71,21 +81,26 @@ def parse_args():
 
 
 def SaveCheckpoint(models, opt_G, i, args, best=False):
+    # Используем безопасное имя для путей
+    exp_dir = 'checkpoint/{}'.format(args.safe_exp_name)
+    if not os.path.isdir(exp_dir):
+        os.makedirs(exp_dir, exist_ok=True)
+
     if best:
         for stage, model in enumerate(models):
             if isinstance(model, nn.DataParallel):
-                torch.save(model.module.state_dict(), 'checkpoint/{}/model_G_S{}_best.pth'.format(args.exp_name, stage))
+                torch.save(model.module.state_dict(), '{}/model_G_S{}_best.pth'.format(exp_dir, stage))
             else:
-                torch.save(model.state_dict(), 'checkpoint/{}/model_G_S{}_best.pth'.format(args.exp_name, stage))
-        torch.save(opt_G.state_dict(), 'checkpoint/{}/opt_G_best.pth'.format(args.exp_name))
-        print("Best checkpoint saved")
+                torch.save(model.state_dict(), '{}/model_G_S{}_best.pth'.format(exp_dir, stage))
+        torch.save(opt_G.state_dict(), '{}/opt_G_best.pth'.format(exp_dir))
+        print("Best checkpoint saved to {}".format(exp_dir))
     else:
         for stage, model in enumerate(models):
             if isinstance(model, nn.DataParallel):
-                torch.save(model.module.state_dict(), 'checkpoint/{}/model_G_S{}_i{:06d}.pth'.format(args.exp_name, stage, i))
+                torch.save(model.module.state_dict(), '{}/model_G_S{}_i{:06d}.pth'.format(exp_dir, stage, i))
             else:
-                torch.save(model.state_dict(), 'checkpoint/{}/model_G_S{}_i{:06d}.pth'.format(args.exp_name, stage, i))
-        torch.save(opt_G.state_dict(), 'checkpoint/{}/opt_G_i{:06d}.pth'.format(args.exp_name, i))
+                torch.save(model.state_dict(), '{}/model_G_S{}_i{:06d}.pth'.format(exp_dir, stage, i))
+        torch.save(opt_G.state_dict(), '{}/opt_G_i{:06d}.pth'.format(exp_dir, i))
         print("Checkpoint saved {}".format(str(i)))
 
 
@@ -95,63 +110,66 @@ if __name__ == "__main__":
     seed_everything(args.seed)
 
     ### Tensorboard for monitoring ###
-    writer = SummaryWriter(log_dir='./log/{}'.format(args.exp_name))
+    # Используем безопасное имя для директории логов
+    writer = SummaryWriter(log_dir='./log/{}'.format(args.safe_exp_name))
 
     models = []
     n_stages = len(args.upscale)
     sr_scale = np.prod(args.upscale)
-    
+
     for s in args.upscale:
         models.append(HKNet(msb=args.msb, lsb=args.lsb, nf=args.n_filters, upscale=s, act=args.act_fn).to(device))
 
-
     ## Optimizers
-    opt_G = optim.Adam([{'params': list(filter(lambda p: p.requires_grad, model.parameters()))} for model in models], 
+    opt_G = optim.Adam([{'params': list(filter(lambda p: p.requires_grad, model.parameters()))} for model in models],
                        lr=args.lr, betas=(0.9, 0.999), weight_decay=args.wd, eps=1e-8, amsgrad=False)
 
     scheduler = optim.lr_scheduler.MultiStepLR(opt_G, milestones=[100000, 150000], gamma=0.1)
 
     ## Load saved params
     if args.start_iter > 0:
+        exp_dir = 'checkpoint/{}'.format(args.safe_exp_name)
         for stage in range(n_stages):
-            lm = torch.load('checkpoint/{}/model_G_S{}_i{:06d}.pth'.format(args.exp_name, stage, args.start_iter))
+            lm = torch.load('{}/model_G_S{}_i{:06d}.pth'.format(exp_dir, stage, args.start_iter))
             models[0].load_state_dict(lm, strict=True)
 
-        lm = torch.load('checkpoint/{}/opt_G_i{:06d}.pth'.format(args.exp_name, args.start_iter))
+        lm = torch.load('{}/opt_G_i{:06d}.pth'.format(exp_dir, args.start_iter))
         opt_G.load_state_dict(lm)
-    
+
     if torch.cuda.device_count() > 1:
         models = [nn.DataParallel(model) for model in models]
-    
+
     # Prepare degradation parameters
     degradation_params = {'sigma': args.noise_std, 'quality': args.jpeg_quality}
-    
-    # Training dataset
-    train_loader = Provider(args.batch_size, args.n_workers, sr_scale, args.train_dir, 
-                           args.crop_size, degradation=args.degradation, 
-                           degradation_params=degradation_params)
 
-    # Validation dataset - for color correction we use same scale (1)
-    valid_loader = SRBenchmark(args.val_dir, scale=sr_scale)
-    valid_datasets = ['Set5']
+    # Training dataset
+    train_loader = Provider(args.batch_size, args.n_workers, sr_scale, args.train_dir,
+                            args.crop_size, degradation=args.degradation,
+                            degradation_params=degradation_params)
+
+    # Validation dataset
+    # Для валидации загружаем чистые изображения (GT).
+    # Деградацию применим вручную в цикле валидации.
+    valid_loader = SRBenchmark(args.val_dir, scale=1)  # scale=1, так как размеры совпадают
+    valid_datasets = ['Set5', 'Set14']  # Добавили Set14 для проверки
 
     ## Prepare directories
     if not os.path.isdir('checkpoint'):
         os.mkdir('checkpoint')
-    if not os.path.isdir('checkpoint/{}'.format(args.exp_name)):
-        os.mkdir('checkpoint/{}'.format(args.exp_name))
+    # Директория создается внутри SaveCheckpoint, но можно создать и здесь
+    if not os.path.isdir('checkpoint/{}'.format(args.safe_exp_name)):
+        os.mkdir('checkpoint/{}'.format(args.safe_exp_name))
     if not os.path.isdir('log'):
         os.mkdir('log')
 
-    l_accum = [0.,0.,0.]
+    l_accum = [0., 0., 0.]
     dT = 0.
     rT = 0.
     accum_samples = 0
 
-
     ### TRAINING
     best_psnr = 0.0
-    for i in tqdm(range(args.start_iter+1, args.train_iter+1)):
+    for i in tqdm(range(args.start_iter + 1, args.train_iter + 1)):
 
         for model in models:
             model.train()
@@ -159,11 +177,10 @@ if __name__ == "__main__":
         # Data preparing
         st = time.time()
         batch_L, batch_H = train_loader.next()
-        batch_H = batch_H.to(device)      # BxCxHxW (32, 3, 192, 192), range [0,1]
-        batch_L = batch_L.to(device)      # BxCxHxW (32, 3, 48, 48), range [0,1]
-    
-        dT += time.time() - st
+        batch_H = batch_H.to(device)  # BxCxHxW, range [0,1]
+        batch_L = batch_L.to(device)  # BxCxHxW, range [0,1] (уже деградировано в Provider)
 
+        dT += time.time() - st
 
         ## TRAIN G
         st = time.time()
@@ -172,7 +189,7 @@ if __name__ == "__main__":
         x = batch_L
         for model in models:
             x = model(x)
-        pred = torch.clamp(x, 0, 1)  # [-2, 2] -> [0, 1]
+        pred = torch.clamp(x, 0, 1)
         loss_G = F.mse_loss(pred, batch_H)
 
         # Update
@@ -186,21 +203,18 @@ if __name__ == "__main__":
         accum_samples += args.batch_size
         l_accum[0] += loss_G.item()
 
-
         ## Show information
         if i % args.i_display == 0:
-            writer.add_scalar('loss_Pixel', l_accum[0]/args.i_display, i)
+            writer.add_scalar('loss_Pixel', l_accum[0] / args.i_display, i)
             print("{}| Iter:{:6d}, Sample:{:6d}, GPixel:{:.2e}, dT:{:.4f}, rT:{:.4f}".format(
-                args.exp_name, i, accum_samples, l_accum[0]/args.i_display, dT/args.i_display, rT/args.i_display))
-            l_accum = [0.,0.,0.]
+                args.exp_name, i, accum_samples, l_accum[0] / args.i_display, dT / args.i_display, rT / args.i_display))
+            l_accum = [0., 0., 0.]
             dT = 0.
             rT = 0.
-
 
         ## Save models
         if i % args.i_save == 0:
             SaveCheckpoint(models, opt_G, i, args)
-
 
         ## Validation
         if i % args.i_validate == 0:
@@ -208,42 +222,66 @@ if __name__ == "__main__":
                 for model in models:
                     model.eval()
 
-
                 for j in range(len(valid_datasets)):
                     psnrs = []
-                    files = valid_loader.files[valid_datasets[j]]
+                    files = valid_loader.files.get(valid_datasets[j], [])
+
+                    if not files:
+                        print(f"Dataset {valid_datasets[j]} not found in validation loader.")
+                        continue
 
                     for k in range(len(files)):
-                        key = valid_datasets[j] + '_' + files[k][:-4]
+                        fname = files[k][:-4]  # remove .png/.jpg
+                        key = valid_datasets[j] + '_' + fname
 
-                        img_gt = valid_loader.ims[key] # (512, 512, 3) range [0, 255]
-                        input_im = valid_loader.ims[key + 'x%d' % sr_scale] # (128, 128, 3) range [0, 255]
+                        # Загружаем GT (чистое изображение)
+                        if key not in valid_loader.ims:
+                            continue
 
-                        input_im = input_im.astype(np.float32) / 255.0  
-                        val_L = torch.Tensor(np.expand_dims(np.transpose(input_im, [2, 0, 1]), axis=0)).to(device) # (1, 3, 128, 128)
+                        img_gt = valid_loader.ims[key]  # (H, W, 3) range [0, 255]
+
+                        # === КЛЮЧЕВОЕ ИЗМЕНЕНИЕ ДЛЯ ЦВЕТОКОРРЕКЦИИ ===
+                        # Генерируем деградированный вход из GT прямо здесь
+                        img_degraded = degrade_image(img_gt, args.degradation, **degradation_params)
+
+                        # Нормализация и подготовка тензора
+                        input_im = img_degraded.astype(np.float32) / 255.0
+                        val_L = torch.Tensor(np.expand_dims(np.transpose(input_im, [2, 0, 1]), axis=0)).to(
+                            device)  # (1, 3, H, W)
 
                         x = val_L
                         for model in models:
                             x = model(x)
 
-
-                        # Output 
-                        image_out = (x).cpu().data.numpy() # (1, 3, 512, 512)
-                        image_out = np.transpose(np.clip(image_out[0], 0. , 1.), [1,2,0]) # BxCxHxW -> HxWxC
-                        image_out = ((image_out)*255).astype(np.uint8)
+                        # Output
+                        image_out = (x).cpu().data.numpy()
+                        image_out = np.transpose(np.clip(image_out[0], 0., 1.), [1, 2, 0])  # HxWxC
+                        image_out = ((image_out) * 255).astype(np.uint8)
 
                         # PSNR on Y channel
-                        psnrs.append(PSNR(_rgb2ycbcr(img_gt)[:,:,0], _rgb2ycbcr(image_out)[:,:,0], sr_scale))
+                        # Для upscale=1 кроп не нужен, размеры должны совпадать
+                        h, w, _ = img_gt.shape
+                        out_h, out_w, _ = image_out.shape
 
-                    mean_psnr = np.mean(np.asarray(psnrs))
+                        # Небольшая страховка от расхождений в размерах на 1 пиксель
+                        min_h, min_w = min(h, out_h), min(w, out_w)
 
-                    # save best psnr for set5
-                    if mean_psnr > best_psnr:
-                        best_psnr = np.mean(np.asarray(psnrs))
-                        SaveCheckpoint(models, opt_G, i, args, best=True)
-                    
-                    print('Iter {} | Dataset {} | AVG Val PSNR: {:02f}'.format(i, valid_datasets[j], mean_psnr))
-                    writer.add_scalar('PSNR_valid/{}'.format(valid_datasets[j]), mean_psnr, i)
+                        psnrs.append(PSNR(_rgb2ycbcr(img_gt[:min_h, :min_w])[:, :, 0],
+                                          _rgb2ycbcr(image_out[:min_h, :min_w])[:, :, 0], 1))
+
+                    if len(psnrs) > 0:
+                        mean_psnr = np.mean(np.asarray(psnrs))
+
+                        # save best psnr
+                        if mean_psnr > best_psnr:
+                            best_psnr = mean_psnr
+                            SaveCheckpoint(models, opt_G, i, args, best=True)
+
+                        print('Iter {} | Dataset {} | AVG Val PSNR: {:.2f}'.format(i, valid_datasets[j], mean_psnr))
+                        writer.add_scalar('PSNR_valid/{}'.format(valid_datasets[j]), mean_psnr, i)
+                    else:
+                        print(f'Iter {i} | Dataset {valid_datasets[j]} | No images processed')
+
                     writer.flush()
 
     print(f'Best PSNR: {best_psnr}')
